fe465246f4708daff01a9d493d091018458d2ef7
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2022-12-04 20:08:39 +0200

    Add files via upload

--- mqtt_subscriber/src/main.c
+++ mqtt_subscriber/src/main.c
@@ -30,27 +30,41 @@ void on_message(struct mosquitto* mosq, void* obj, const struct mosquitto_messag
     printf("New message with topic: %s: %s\n", msg->topic, (char*) msg->payload);
 }
 
+int init_mosquitto(struct mosquitto** mosq, int* id)
+{
+    int rc = 0;
+    mosquitto_lib_init();
+    *mosq = mosquitto_new("subscribe-test", true, id);
+    mosquitto_connect_callback_set(*mosq, on_connect);
+    mosquitto_message_callback_set(*mosq, on_message);
+    rc = mosquitto_connect(*mosq, "localhost", 1883, 10);
+    mosquitto_loop_start(*mosq);
+    return rc;
+}
+
+void end_mosquitto(struct mosquitto** mosq)
+{
+    mosquitto_loop_stop(*mosq, true);
+    mosquitto_disconnect(*mosq);
+    mosquitto_destroy(*mosq);
+    mosquitto_lib_cleanup();
+}
+
+
 int main(int argc, char* argv[])
 {
     int rc=0;
     int id = 12;
 
-    mosquitto_lib_init();
     struct mosquitto* mosq;
-    mosq = mosquitto_new("subscribe-test", true, &id);
-    mosquitto_connect_callback_set(mosq, on_connect);
-    mosquitto_message_callback_set(mosq, on_message);
 
-    rc = mosquitto_connect(mosq, "localhost", 1883, 10);
-    if(rc){
-        printf("Could not connect to broker with RC=%d",rc);
-    }
 
-    mosquitto_loop_start(mosq);
+    init_mosquitto(&mosq, &id);
+
+
     printf("Press enter to stop \n");
     getchar();
-    mosquitto_loop_stop(mosq, true);
-    mosquitto_disconnect(mosq);
-    mosquitto_destroy(mosq);
-    mosquitto_lib_cleanup();
+
+    end_mosquitto(&mosq);
+
 }
